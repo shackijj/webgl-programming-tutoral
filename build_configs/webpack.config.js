2d4191c6d5a52a642d23e0eb7c717fb1cc7f2713
Kirill Shatskiy <shaackiy@gmail.com>
2018-06-05 17:24:48 +0300

    infra: use HtmlWebpackPlugin for dynamically create pages

--- build_configs/webpack.config.js
+++ build_configs/webpack.config.js
@@ -1,19 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
-        ch1: './src/ch1/index.js',
-        ch2: './src/ch2/index.js',
+        'ch1': './src/ch1/index.js',
+        'ch2': './src/ch2/index.js',
     },
     output: {
-        path: path.resolve(__dirname, '../public/assets'),
-        filename: '[name].bundle.js'
+        path: path.resolve(__dirname, '../public/'),
+        filename: 'js/[name].bundle.js'
     },
     devServer: {
         contentBase: path.join(__dirname, '../public'),
-        publicPath: '/assets/',
+        publicPath: '/',
         port: 9000,
         open: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: false,
+            chunks: ['ch1'],
+            bundle: 'ch1',
+            filename: 'ch1/index.html',
+            template: 'src/template.html',
+          }),
+          new HtmlWebpackPlugin({
+            inject: false,
+            chunks: ['ch2'],
+            bundle: 'ch2',
+            filename: 'ch2/index.html',
+            template: 'src/template.html'
+          })
+    ]
 }
