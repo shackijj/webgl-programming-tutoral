f76ab04466deae1f57792282378c471cb348a35c
Kirill Shatskiy <shaackiy@gmail.com>
2018-06-07 11:55:00 +0300

    feat: make HelloCanvas working

--- src/ch1/HelloCanvas.js
+++ src/ch1/HelloCanvas.js
@@ -1,9 +1,18 @@
+const cuonUtils = require('../../lib/cuon-utils');
+
 /**
  * This main function of the app
  * @return {void}
  */
 function main() {
-    alert('test');
+    const canvas = document.getElementById('webgl');
+    const gl = cuonUtils.getWebGLContext(canvas);
+    if (!gl) {
+        throw new Error('Failed to get the rendering context for WebGL');
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 module.exports = main;
