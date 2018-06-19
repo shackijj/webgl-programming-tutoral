71c00f72cea48df33a5ba66bd5caf831779c9691
Kirill Shatskiy <shaackiy@gmail.com>
2018-06-19 10:26:00 +0300

    feat: complete the MultiPoints example

--- build_configs/webpack.config.js
+++ build_configs/webpack.config.js
@@ -4,11 +4,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        'HelloCanvas': './src/ch1/HelloCanvas.js',
-        'HelloPoint1': './src/ch1/HelloPoint1/HelloPoint1.js',
-        'HelloPoint2': './src/ch1/HelloPoint2/HelloPoint2.js',
-        'ClickPoint': './src/ch1/ClickPoint/ClickPoint.js',
-        'ColoredPoints': './src/ch1/ColoredPoints/ColoredPoints.js',
+        'HelloCanvas': './src/ch2/HelloCanvas.js',
+        'HelloPoint1': './src/ch2/HelloPoint1/HelloPoint1.js',
+        'HelloPoint2': './src/ch2/HelloPoint2/HelloPoint2.js',
+        'ClickPoint': './src/ch2/ClickPoint/ClickPoint.js',
+        'ColoredPoints': './src/ch2/ColoredPoints/ColoredPoints.js',
+        'MultiPoints': './src/ch3/MultiPoints/MultiPoints.js',
     },
     output: {
         path: path.resolve(__dirname, '../public/'),
@@ -63,6 +64,13 @@ module.exports = {
             chunks: ['ColoredPoints'],
             bundle: 'ColoredPoints',
             filename: 'ch1/ColoredPoints.html',
+            template: 'src/template.html',
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            chunks: ['MultiPoints'],
+            bundle: 'MultiPoints',
+            filename: 'ch2/MultiPoints.html',
             template: 'src/template.html',
         }),
     ]
